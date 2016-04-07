e24b7c96b7c173f7055ea851f33d95ed31f8c3f6
Aleksander Korolev <a_korolev@wargaming.net>
2016-04-07 10:20:47 +0300

    add check existing destination bundle

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ function PolyfillsPlugin(options) {
 }
 
 PolyfillsPlugin.prototype.apply = function(compiler) {
-  compiler.plugin("emit", function(compilation, callback) {
-    if (this.options.ObjectDefineProperty) {
-        ObjectDefinePropertyPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Object/defineProperty/polyfill.js'), 'utf8') || '';
-        compilation.assets[this.options.bundle]._source.children.unshift(ObjectDefinePropertyPolyfill);
-    }
-    if (this.options.JSON) {
-        JSONPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/JSON/polyfill.js'), 'utf8') || '';
-        compilation.assets[this.options.bundle]._source.children.unshift(JSONPolyfill);
-    }
-    if (this.options.html5Elements) {
-        JSONPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/~html5-elements/polyfill.js'), 'utf8') || '';
-        compilation.assets[this.options.bundle]._source.children.unshift(JSONPolyfill);
-    }
-    callback();
-  });
+    compiler.plugin("emit", function(compilation, callback) {
+        if (compilation.assets[this.options.bundle]) {
+            if (this.options.ObjectDefineProperty) {
+                ObjectDefinePropertyPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Object/defineProperty/polyfill.js'), 'utf8') || '';
+                compilation.assets[this.options.bundle]._source.children.unshift(ObjectDefinePropertyPolyfill);
+            }
+            if (this.options.JSON) {
+                JSONPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/JSON/polyfill.js'), 'utf8') || '';
+                compilation.assets[this.options.bundle]._source.children.unshift(JSONPolyfill);
+            }
+            if (this.options.html5Elements) {
+                JSONPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/~html5-elements/polyfill.js'), 'utf8') || '';
+                compilation.assets[this.options.bundle]._source.children.unshift(JSONPolyfill);
+            }
+            callback();
+        }
+    });
 };
 
 module.exports = PolyfillsPlugin;
