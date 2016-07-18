a4c08b3d002290fb3c0e536968c05108521c6771
Aleksander Korolev <a_korolev@wargaming.net>
2016-07-18 16:51:18 +0300

    reorder include polyfills

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function PolyfillsPlugin(options) {
 PolyfillsPlugin.prototype.apply = function(compiler) {
     compiler.plugin("emit", function(compilation, callback) {
         if (compilation.assets[PolyfillsPlugin.options.bundle]) {
+            if (PolyfillsPlugin.options.bind) {
+                bindPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Function/prototype/bind/polyfill.js'), 'utf8') || '';
+                compilation.assets[PolyfillsPlugin.options.bundle]._source.children.unshift(bindPolyfill);
+            }
             if (PolyfillsPlugin.options.ObjectDefineProperty) {
                 ObjectDefinePropertyPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Object/defineProperty/polyfill.js'), 'utf8') || '';
                 compilation.assets[PolyfillsPlugin.options.bundle]._source.children.unshift(ObjectDefinePropertyPolyfill);
@@ -27,10 +31,6 @@ PolyfillsPlugin.prototype.apply = function(compiler) {
             if (PolyfillsPlugin.options.indexOf) {
                 indexOfPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Array/prototype/indexOf/polyfill.js'), 'utf8') || '';
                 compilation.assets[PolyfillsPlugin.options.bundle]._source.children.unshift(indexOfPolyfill);
-            }
-            if (PolyfillsPlugin.options.bind) {
-                bindPolyfill = fs.readFileSync(require.resolve('polyfill-service/polyfills/Function/prototype/bind/polyfill.js'), 'utf8') || '';
-                compilation.assets[PolyfillsPlugin.options.bundle]._source.children.unshift(bindPolyfill);
             }
             callback();
         }
